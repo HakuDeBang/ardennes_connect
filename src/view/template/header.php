c7d2893692ae4060c5aab661cd3ee1415c64d94b
HakuDeBang <mielotr.dev@gmail.com>
2023-10-03 16:00:04 +0200

    header tailwind

--- src/view/template/header.php
+++ src/view/template/header.php
@@ -2,8 +2,11 @@
     <img class="brightness-[0.75] h-[800px] w-[1600px] rounded-[40px]" src="src/assets/img/bg-ardennes.jpg" alt="Forêt Ardennes">
     <div class="absolute top-0 left-0 w-full h-full flex items-start">
         <div class="flex-grow flex flex-col item-center justify-center">
-            <p class="ml-16 mt-24 text-lightGray font-semibold text-6xl leading-[85px]">Découvrez<br>
-                <span class="text-8xl">ArdenneConnect</span><br>
+            <p class="ml-16 mt-24 text-lightGray font-semibold text-6xl leading-[85px]">
+                Découvrez<br>
+            </p>
+            <h1 class="ml-16 text-lightGray font-semibold text-8xl mb-7">ArdenneConnect</h1>
+            <p class="ml-16 text-lightGray font-semibold leading-[85px]">
                 <span class="text-6xl">Une application web<br>
                 regroupant toutes les activités,</span><br>
                 <span class="text-4xl">loisirs, restaurants, évènements,...</span><br> 
