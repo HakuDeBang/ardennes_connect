9ec0fa329c394129a43d8441428225d1ec3c30ef
HakuDeBang <mielotr.dev@gmail.com>
2023-10-03 13:44:42 +0200

    navbar tailwind

--- src/view/home.php
+++ src/view/home.php
@@ -5,12 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ardennes Connect</title>
     <link href="./dist/output.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="src/assets/svg/favicon.svg">
+    <link rel="icon" type="image/png" sizes="32x32" href="src/assets/svg/favicon.svg">
+    <link rel="icon" type="image/png" sizes="16x16" href="src/assets/svg/favicon.svg">
 </head>
 <body>
-    
-<?php require('template/navbar.php'); ?>
-<main></main>
-<?php require('template/footer.php'); ?>
-
+    <section class="bg-darkGray flex flex-col items-center justify-center">
+        <?php require('template/navbar.php'); ?>
+        
+        <?php require('template/header.php'); ?>
+    </section>
+    <?php require('template/footer.php'); ?>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'src/view/*.php',
-    'src/view/template/*.php'
-  ],
+  content: ["src/view/*.php", "src/view/template/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#004a93",
+        lightBlue: " #00abe7",
+        yellowLogo: "#ffdf00",
+        redLogo: "#dc2f34",
+        lightGray: "#F1F1F1",
+        darkGray: "#CAC6C6"
+      },
+      fontFamily: {
+        league: ['League Spartan'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
