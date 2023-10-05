8e2507acea131ca3c98eae69002536eba67cc1ff
SirLauvel <84542996+SirLauvel@users.noreply.github.com>
2023-10-05 11:57:22 +0200

    navbar_v2

--- src/view/template/navbar.php
+++ src/view/template/navbar.php
@@ -1,11 +1,11 @@
-<nav class="sticky top-0 mx-auto bg-lightGray w-10/12 rounded-b-[40px]">
-    <div class="flex flex-row items-center justify-between">
+<nav class="hidden lg:block sticky top-0 mx-auto bg-lightGray w-10/12 rounded-b-[40px]">
+    <div class="px-8 py-3 flex flex-row items-center justify-between">
         <!-- logo -->
-        <a href="#" class="flex mx-14">
-            <img class="w-[60px] h-[60px]" src="src/assets/img/logo.png" alt="Logo Ardennes Connect">
+        <a href="#" class="">
+            <img class="w-[50px] h-[50px]" src="src/assets/img/logo2.png" alt="Logo Ardennes Connect">
         </a>
         <!-- Bouton Navbar -->
-        <div class="flex flex-row items-center space-x-16">
+        <div class="flex-grow flex justify-center items-center gap-20">
             <a href="accueil" class="text-lg font-semibold hover:underline hover:underline-offset-8 hover:decoration hover:decoration-4 hover:decoration-darkBlue">Accueil</a>
             <a href="evenement" class="text-lg font-semibold hover:underline hover:underline-offset-8 hover:decoration hover:decoration-4 hover:decoration-lightBlue">Évènements</a>
             <a href="carte" class="text-lg font-semibold hover:underline hover:underline-offset-8 hover:decoration hover:decoration-4 hover:decoration-yellowLogo">Carte</a>
@@ -14,9 +14,9 @@
         <!-- Bouton Utilisateur -->
         <?php
         if (isset($_SESSION['user'])) { ?>
-            <a href="profil" class="bg-lightBlue rounded-lg text-lg text-center font-semibold py-1.5 mx-14 w-40 text-lightGray">Mon Compte</a>
+            <a href="profil" class="bg-lightBlue rounded-lg text-lg text-center font-semibold py-1.5 px-3 text-lightGray">Mon Compte</a>
         <?php } else { ?>
-            <a href="connexion" class="bg-lightBlue rounded-lg text-lg text-center font-semibold py-1.5 mx-14 w-40 text-lightGray">Connexion</a>
+            <a href="connexion" class="bg-lightBlue rounded-lg text-lg text-center font-semibold py-1.5 px-3 text-lightGray">Connexion</a>
         <?php } ?>
     </div>
 </nav>
