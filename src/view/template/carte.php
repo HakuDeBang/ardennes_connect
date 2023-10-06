2e3b16c0733fe99af354e6ab9b340f402d821ffa
SirLauvel <84542996+SirLauvel@users.noreply.github.com>
2023-10-06 15:28:51 +0200

    fix_button_home

--- src/view/template/carte.php
+++ src/view/template/carte.php
@@ -8,9 +8,11 @@
             <p class="flex items-center text-center lg:text-left ml-4 2xl:ml-6 text-base 2xl:text-2xl font-normal">amet, tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimconsectetur adipiscin</p>
             <h4 class="flex items-center text-center lg:text-left ml-4 2xl:ml-6 text-lg 2xl:text-3xl font-medium">Lorem ipsum dolor sit amet, consectetur adipiscing ?</h4>
             <p class="flex items-center text-center lg:text-left ml-4 2xl:ml-6 text-base 2xl:text-2xl font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut en Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud im ad minim veniam, quis nostrud</p>
-            <a href="carte" class="flex items-center justify-center w-44 text-sm px-4 py-1 2xl:w-56 2xl:text-xl rounded-full ml-4 2xl:ml-20 2xl:px-8 2xl:py-2 bg-lightBlue text-lightGray">
-                Découvrir la carte
-            </a>
+            <div class="flex items-center justify-center">
+                <a href="carte" class="w-44 text-sm px-4 py-1 2xl:w-56 2xl:text-xl rounded-full ml-4 2xl:ml-20 2xl:px-8 2xl:py-2 bg-lightBlue text-lightGray">
+                    Découvrir la carte
+                </a>
+            </div>
         </div>
         <div class="flex items-center justify-center">
             <img class="hidden lg:flex lg:w-96 xl:w-auto flex items-center justify-center" src="src/assets/img/mockup.png" alt="Mockup Iphone">
